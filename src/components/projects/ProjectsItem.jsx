697312c8a495a50d0ae7981e1d39705a3005ed48
Kodukula Satya Gopal <108046917+SatyagopalK@users.noreply.github.com>
2025-12-25 18:08:40 +0530

    Update ProjectsItem.jsx

--- src/components/projects/ProjectsItem.jsx
+++ src/components/projects/ProjectsItem.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function ProjectsItem({ book, index, id }){
     const bookId = typeof id !== 'undefined' ? id : (book && typeof book.id !== 'undefined' ? book.id : index)
     return (
-        <article className="mx-auto flex flex-col md:flex-row items-center gap-8 justify-left w-[85vw]">
+        <article className="mx-auto flex flex-col md:flex-row items-center gap-8 justify-center w-[85vw]">
         <div className="flex-shrink-0 w-full md:w-1/3">
                 <Link to={`/books/${bookId}`}>
                     <img src={book.img} alt={book.title} className="w-full h-auto rounded shadow-sm" />
@@ -12,7 +12,7 @@ export default function ProjectsItem({ book, index, id }){
             </div>
             <div className="w-full md:w-2/3">
                 <h2 className="text-2xl font-semibold mb-2 "><Link to={`/books/${bookId}`} className="hover:underline">{book.title}</Link></h2>
-                <p className="text-gray-700 mb-4 text-justify">{book.desc}</p>
+                <p className="text-gray-700 mb-4 text-left">{book.desc}</p>
                 <Link to={`/projects/${bookId}`} className="btn">Learn More</Link>
             </div>
         </article>
