37caf53b7c15cbd6530339ca2eb65a47f6dd7028
Kodukula Satya Gopal <108046917+SatyagopalK@users.noreply.github.com>
2025-12-25 18:01:43 +0530

    Update ProjectsItem.jsx

--- src/components/projects/ProjectsItem.jsx
+++ src/components/projects/ProjectsItem.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function ProjectsItem({ book, index, id }){
     const bookId = typeof id !== 'undefined' ? id : (book && typeof book.id !== 'undefined' ? book.id : index)
     return (
-        <article className="mx-auto flex flex-col md:flex-row items-center gap-8 justify-center w-[85vw]">
+        <article className="mx-auto flex flex-col md:flex-row items-center gap-8 justify-left w-[85vw]">
         <div className="flex-shrink-0 w-full md:w-1/3">
                 <Link to={`/books/${bookId}`}>
                     <img src={book.img} alt={book.title} className="w-full h-auto rounded shadow-sm" />
